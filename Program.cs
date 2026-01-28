6f2a37839f61bece6b9f136e954f1ddc299e0734
Patrick McIsaac <patrick.mcisaac.dev@gmail.com>
2026-01-27 19:49:34 -0600

    converted employee get endpoints to use sql

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 using HoneyRaesAPI.Models;
 using HoneyRaesAPI.Models.DTOs;
+using Npgsql;
+using DotNetEnv;
+
+Env.Load();
+
+var connectionString = Env.GetString("POSTGRES_CONNECT");
 
 List<Customer> customers = new List<Customer>
 {
@@ -37,222 +43,263 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// ENDPOINTS
-app.MapGet("/servicetickets", () =>
-{
-    return serviceTickets.Select(t => new ServiceTicketDTO
-    {
-        Id = t.Id,
-        CustomerId = t.CustomerId,
-        EmployeeId = t.EmployeeId,
-        Description = t.Description,
-        Emergency = t.Emergency,
-        DateCompleted = t.DateCompleted
-    });
-});
+// // ENDPOINTS
+// app.MapGet("/servicetickets", () =>
+// {
+//     return serviceTickets.Select(t => new ServiceTicketDTO
+//     {
+//         Id = t.Id,
+//         CustomerId = t.CustomerId,
+//         EmployeeId = t.EmployeeId,
+//         Description = t.Description,
+//         Emergency = t.Emergency,
+//         DateCompleted = t.DateCompleted
+//     });
+// });
 
-app.MapGet("/servicetickets/{id}", (int id) =>
-{
+// app.MapGet("/servicetickets/{id}", (int id) =>
+// {
 
-    ServiceTicket serviceTicket = serviceTickets.FirstOrDefault(st => st.Id == id);
+//     ServiceTicket serviceTicket = serviceTickets.FirstOrDefault(st => st.Id == id);
 
-    if (serviceTicket == null)
-    {
-        return Results.NotFound();
-    }
-    Employee employee = employees.FirstOrDefault(e => e.Id == serviceTicket.EmployeeId);
+//     if (serviceTicket == null)
+//     {
+//         return Results.NotFound();
+//     }
+//     Employee employee = employees.FirstOrDefault(e => e.Id == serviceTicket.EmployeeId);
 
-    Customer customer = customers.FirstOrDefault(c => c.Id == serviceTicket.CustomerId);
+//     Customer customer = customers.FirstOrDefault(c => c.Id == serviceTicket.CustomerId);
 
 
-    return Results.Ok(new ServiceTicketDTO
-    {
-        Id = serviceTicket.Id,
-        CustomerId = serviceTicket.CustomerId,
-        Customer = customer == null ? null : new CustomerDTO
-        {
-            Id = customer.Id,
-            Name = customer.Name,
-            Address = customer.Address
-        },
-        EmployeeId = serviceTicket.EmployeeId,
-        Description = serviceTicket.Description,
-        Emergency = serviceTicket.Emergency,
-        DateCompleted = serviceTicket.DateCompleted,
-        Employee = employee == null ? null : new EmployeeDTO
-        {
-            Id = employee.Id,
-            Name = employee.Name,
-            Specialty = employee.Specialty,
+//     return Results.Ok(new ServiceTicketDTO
+//     {
+//         Id = serviceTicket.Id,
+//         CustomerId = serviceTicket.CustomerId,
+//         Customer = customer == null ? null : new CustomerDTO
+//         {
+//             Id = customer.Id,
+//             Name = customer.Name,
+//             Address = customer.Address
+//         },
+//         EmployeeId = serviceTicket.EmployeeId,
+//         Description = serviceTicket.Description,
+//         Emergency = serviceTicket.Emergency,
+//         DateCompleted = serviceTicket.DateCompleted,
+//         Employee = employee == null ? null : new EmployeeDTO
+//         {
+//             Id = employee.Id,
+//             Name = employee.Name,
+//             Specialty = employee.Specialty,
 
-        }
-    });
+//         }
+//     });
 
-});
+// });
 
-app.MapPost("/servicetickets", (ServiceTicket serviceTicket) =>
-{
-    // Get the customer data to check that the customerid for the service ticket is valid
-    Customer customer = customers.FirstOrDefault(c => c.Id == serviceTicket.CustomerId);
+// app.MapPost("/servicetickets", (ServiceTicket serviceTicket) =>
+// {
+//     // Get the customer data to check that the customerid for the service ticket is valid
+//     Customer customer = customers.FirstOrDefault(c => c.Id == serviceTicket.CustomerId);
 
-    // If the client did not provide a valid customer id, this is a bad request
-    if (customer == null)
-    {
-        return Results.BadRequest();
-    }
+//     // If the client did not provide a valid customer id, this is a bad request
+//     if (customer == null)
+//     {
+//         return Results.BadRequest();
+//     }
 
-    // creates a new id (SQL will do this for us like JSON server)
-    serviceTicket.Id = serviceTickets.Max(st => st.Id) + 1;
-    serviceTickets.Add(serviceTicket);
+//     // creates a new id (SQL will do this for us like JSON server)
+//     serviceTicket.Id = serviceTickets.Max(st => st.Id) + 1;
+//     serviceTickets.Add(serviceTicket);
 
-    return Results.Created($"/servicetickets/{serviceTicket.Id}", new ServiceTicketDTO
-    {
-        Id = serviceTicket.Id,
-        CustomerId = serviceTicket.CustomerId,
-        Customer = new CustomerDTO
-        {
-            Id = customer.Id,
-            Name = customer.Name,
-            Address = customer.Address
-        },
-        Description = serviceTicket.Description,
-        Emergency = serviceTicket.Emergency
-    });
-});
+//     return Results.Created($"/servicetickets/{serviceTicket.Id}", new ServiceTicketDTO
+//     {
+//         Id = serviceTicket.Id,
+//         CustomerId = serviceTicket.CustomerId,
+//         Customer = new CustomerDTO
+//         {
+//             Id = customer.Id,
+//             Name = customer.Name,
+//             Address = customer.Address
+//         },
+//         Description = serviceTicket.Description,
+//         Emergency = serviceTicket.Emergency
+//     });
+// });
 
-app.MapPost("/servicetickets/{id}/complete", (int id) =>
-{
-    ServiceTicket serviceTicket = serviceTickets.FirstOrDefault(st => st.Id == id);
+// app.MapPost("/servicetickets/{id}/complete", (int id) =>
+// {
+//     ServiceTicket serviceTicket = serviceTickets.FirstOrDefault(st => st.Id == id);
 
-    if (serviceTicket == null)
-    {
-        return Results.NotFound();
-    }
+//     if (serviceTicket == null)
+//     {
+//         return Results.NotFound();
+//     }
 
-    serviceTicket.DateCompleted = DateTime.Today;
-    return Results.NoContent();
-});
+//     serviceTicket.DateCompleted = DateTime.Today;
+//     return Results.NoContent();
+// });
 
-app.MapDelete("/servicetickets/{id}", (int id) =>
-{
-    ServiceTicket serviceTicket = serviceTickets.FirstOrDefault(st => st.Id == id);
-    if (serviceTicket == null)
-    {
-        return Results.BadRequest();
-    }
+// app.MapDelete("/servicetickets/{id}", (int id) =>
+// {
+//     ServiceTicket serviceTicket = serviceTickets.FirstOrDefault(st => st.Id == id);
+//     if (serviceTicket == null)
+//     {
+//         return Results.BadRequest();
+//     }
 
-    serviceTickets.Remove(serviceTicket);
-    return Results.NoContent();
-});
+//     serviceTickets.Remove(serviceTicket);
+//     return Results.NoContent();
+// });
 
-app.MapPut("/servicetickets/{id}", (int id, ServiceTicket serviceTicket) =>
-{
-    ServiceTicket ticketToUpdate = serviceTickets.FirstOrDefault(st => st.Id == id);
+// app.MapPut("/servicetickets/{id}", (int id, ServiceTicket serviceTicket) =>
+// {
+//     ServiceTicket ticketToUpdate = serviceTickets.FirstOrDefault(st => st.Id == id);
 
-    if (ticketToUpdate == null)
-    {
-        return Results.NotFound();
-    }
+//     if (ticketToUpdate == null)
+//     {
+//         return Results.NotFound();
+//     }
 
-    if (id != serviceTicket.Id)
-    {
-        return Results.BadRequest();
-    }
+//     if (id != serviceTicket.Id)
+//     {
+//         return Results.BadRequest();
+//     }
 
-    ticketToUpdate.CustomerId = serviceTicket.CustomerId;
-    ticketToUpdate.EmployeeId = serviceTicket.EmployeeId;
-    ticketToUpdate.Description = serviceTicket.Description;
-    ticketToUpdate.Emergency = serviceTicket.Emergency;
-    ticketToUpdate.DateCompleted = serviceTicket.DateCompleted;
+//     ticketToUpdate.CustomerId = serviceTicket.CustomerId;
+//     ticketToUpdate.EmployeeId = serviceTicket.EmployeeId;
+//     ticketToUpdate.Description = serviceTicket.Description;
+//     ticketToUpdate.Emergency = serviceTicket.Emergency;
+//     ticketToUpdate.DateCompleted = serviceTicket.DateCompleted;
 
-    return Results.NoContent();
-});
+//     return Results.NoContent();
+// });
 
 app.MapGet("/employees", () =>
 {
-    return employees.Select(e => new EmployeeDTO
+    List<Employee> employees = new List<Employee>();
+
+    using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+
+    connection.Open();
+
+    using NpgsqlCommand command = connection.CreateCommand();
+    command.CommandText = "SELECT * FROM Employee";
+
+    using NpgsqlDataReader reader = command.ExecuteReader();
+
+    while (reader.Read())
     {
-        Id = e.Id,
-        Name = e.Name,
-        Specialty = e.Specialty
-    });
+        employees.Add(new Employee
+        {
+            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+            Name = reader.GetString(reader.GetOrdinal("Name")),
+            Specialty = reader.GetString(reader.GetOrdinal("Specialty"))
+        });
+    }
+    return employees;
 });
 
 app.MapGet("/employees/{id}", (int id) =>
 {
-    Employee employee = employees.FirstOrDefault(e => e.Id == id);
-    List<ServiceTicket> tickets = serviceTickets.Where(st => st.EmployeeId == id).ToList();
+    Employee employee = null;
+    using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+    connection.Open();
+    using NpgsqlCommand command = connection.CreateCommand();
+    command.CommandText = @"
+        SELECT 
+            e.Id,
+            e.Name, 
+            e.Specialty, 
+            st.Id AS serviceTicketId, 
+            st.CustomerId,
+            st.Description,
+            st.Emergency,
+            st.DateCompleted 
+        FROM Employee e
+        LEFT JOIN ServiceTicket st ON st.EmployeeId = e.Id
+        WHERE e.Id = @id";
 
-    if (employee == null)
+    command.Parameters.AddWithValue("@id", id);
+
+    using NpgsqlDataReader reader = command.ExecuteReader();
+
+    while (reader.Read())
     {
-        return Results.NotFound();
-    }
-    return Results.Ok(new EmployeeDTO
-    {
-        Id = employee.Id,
-        Name = employee.Name,
-        Specialty = employee.Specialty,
-        ServiceTickets = tickets.Select(t => new ServiceTicketDTO
+        if (employee == null)
         {
-            Id = t.Id,
-            CustomerId = t.CustomerId,
-            EmployeeId = t.EmployeeId,
-            Description = t.Description,
-            Emergency = t.Emergency,
-            DateCompleted = t.DateCompleted
-        }).ToList()
-    });
-
-});
-
-app.MapGet("/customers", () =>
-{
-    return customers.Select(c => new CustomerDTO
-    {
-        Id = c.Id,
-        Name = c.Name,
-        Address = c.Address
-    });
-});
-
-app.MapGet("/customers/{id}", (int id) =>
-{
-    Customer customer = customers.FirstOrDefault(c => c.Id == id);
-    if (customer == null)
-    {
-        return Results.NotFound();
-    }
-
-    List<ServiceTicket> customerTickets = serviceTickets.Where(st => st.CustomerId == id).ToList();
-    return Results.Ok(new CustomerDTO
-    {
-        Id = customer.Id,
-        Name = customer.Name,
-        Address = customer.Address,
-        ServiceTickets = customerTickets.Select(ct =>
-        {
-            Employee employee = employees.FirstOrDefault(e => e.Id == ct.EmployeeId);
-
-
-            return new ServiceTicketDTO
+            employee = new Employee
             {
-                Id = ct.Id,
-                CustomerId = ct.CustomerId,
-                EmployeeId = ct.EmployeeId,
-                Description = ct.Description,
-                DateCompleted = ct.DateCompleted,
-                Employee = employee == null ? null : new EmployeeDTO
-                {
-                    Id = employee.Id,
-                    Name = employee.Name,
-                    Specialty = employee.Specialty
-                }
+                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                Name = reader.GetString(reader.GetOrdinal("Name")),
+                Specialty = reader.GetString(reader.GetOrdinal("Specialty")),
+                ServiceTickets = new List<ServiceTicket>()
             };
-        }).ToList()
-    });
+        }
 
-
+        if (!reader.IsDBNull(reader.GetOrdinal("serviceTicketId")))
+        {
+            employee.ServiceTickets?.Add(new ServiceTicket
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("serviceTicketId")),
+                CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                EmployeeId = id,
+                Description = reader.GetString(reader.GetOrdinal("Description")),
+                Emergency = reader.GetBoolean(reader.GetOrdinal("Emergency")),
+                DateCompleted = reader.IsDBNull(reader.GetOrdinal("DateCompleted")) ? null : reader.GetDateTime(reader.GetOrdinal("DateCompleted"))
+            });
+        }
+    }
+    return employee == null ? Results.NotFound() : Results.Ok(employee);
 });
+
+// app.MapGet("/customers", () =>
+// {
+//     return customers.Select(c => new CustomerDTO
+//     {
+//         Id = c.Id,
+//         Name = c.Name,
+//         Address = c.Address
+//     });
+// });
+
+// app.MapGet("/customers/{id}", (int id) =>
+// {
+//     Customer customer = customers.FirstOrDefault(c => c.Id == id);
+//     if (customer == null)
+//     {
+//         return Results.NotFound();
+//     }
+
+//     List<ServiceTicket> customerTickets = serviceTickets.Where(st => st.CustomerId == id).ToList();
+//     return Results.Ok(new CustomerDTO
+//     {
+//         Id = customer.Id,
+//         Name = customer.Name,
+//         Address = customer.Address,
+//         ServiceTickets = customerTickets.Select(ct =>
+//         {
+//             Employee employee = employees.FirstOrDefault(e => e.Id == ct.EmployeeId);
+
+
+//             return new ServiceTicketDTO
+//             {
+//                 Id = ct.Id,
+//                 CustomerId = ct.CustomerId,
+//                 EmployeeId = ct.EmployeeId,
+//                 Description = ct.Description,
+//                 DateCompleted = ct.DateCompleted,
+//                 Employee = employee == null ? null : new EmployeeDTO
+//                 {
+//                     Id = employee.Id,
+//                     Name = employee.Name,
+//                     Specialty = employee.Specialty
+//                 }
+//             };
+//         }).ToList()
+//     });
+
+
+// });
 
 
 app.Run();
